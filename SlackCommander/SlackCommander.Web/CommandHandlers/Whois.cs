c7d97af5c8aeda7a4b534bb3a96281b936f95682
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2014-08-27 16:20:35 +0200

    Validation of e-mail address in /whois.

--- SlackCommander/SlackCommander.Web/CommandHandlers/Whois.cs
+++ SlackCommander/SlackCommander.Web/CommandHandlers/Whois.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 
 namespace SlackCommander.Web.CommandHandlers
@@ -9,7 +10,18 @@ namespace SlackCommander.Web.CommandHandlers
     {
         public dynamic Handle(Command command)
         {
-            return "Hello " + command.user_name;
+            try
+            {
+                new MailAddress(command.text);
+            }
+            catch
+            {
+                return string.Format("Sorry, '{0}' does not seem to be a valid e-mail address.", command.text);
+            }
+
+            // TODO
+            // https://api.fullcontact.com/v2/person.json?email=bart@fullcontact.com&apiKey=xxxx
+            return string.Format("Looking up '{0}', just a second...", command.text);
         }
     }
 }
