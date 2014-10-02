1a0f376a62eb4cb33bea2d5f9dbd0f499abe0e7c
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2014-10-02 09:28:52 +0200

    Pad todo item ids to make them equal width.

--- SlackCommander/SlackCommander.Web/Todo/TodoItemExtensions.cs
+++ SlackCommander/SlackCommander.Web/Todo/TodoItemExtensions.cs
@@ -15,23 +15,19 @@ namespace SlackCommander.Web.Todo
             {
                 return "You are all done!";
             }
+            int idMaxLength = items.Max(i => i.Id.Length);
             var text = new StringBuilder();
             foreach (var item in items)
             {
-                text.AppendLine(item.ToSlackString());
+                text.AppendLine(string.Format(
+                "`{0}` {1} {2} {3} {4}",
+                item.Id.PadLeft(idMaxLength),
+                item.Done ? ":white_check_mark:" : ":white_square:",
+                item.Text,
+                item.ClaimedBy.Missing() ? string.Empty : ":lock:",
+                item.ClaimedBy.Missing() ? string.Empty : string.Format("<@{0}>", item.ClaimedBy)).Trim());
             }
             return text.ToString().Trim();
-        }
-
-        public static string ToSlackString(this TodoItem todoItem)
-        {
-            return string.Format(
-                "`{0}` {1} {2} {3} {4}",
-                todoItem.Id,
-                todoItem.Done ? ":white_check_mark:" : ":white_square:",
-                todoItem.Text,
-                todoItem.ClaimedBy.Missing() ? string.Empty : ":lock:",
-                todoItem.ClaimedBy.Missing() ? string.Empty : string.Format("<@{0}>", todoItem.ClaimedBy)).Trim();
         }
     }
 }
